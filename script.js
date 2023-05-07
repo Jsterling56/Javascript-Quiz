8a57682410af047afcb063e0a1e334dff0545bd4
unknown <jtsterlin@yahoo.com>
2023-05-07 16:36:21 -0400

    began project, added starter code HTML, CSS, JS

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("box2").addEventListener('click', changeColor);
+function changeColor(){
+    document.getElementById('box2').innerHTML = 'CLICK!';
+    document.getElementById('box2').style.backgroundColor = "orange";
+};

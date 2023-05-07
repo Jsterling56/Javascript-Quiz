d6fc60f362913b89fdc218ef952b307c94a247f5
unknown <jtsterlin@yahoo.com>
2023-05-07 17:05:14 -0400

    started adding extra JS

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-document.getElementById("box2").addEventListener('click', changeColor);
-function changeColor(){
-    document.getElementById('box2').innerHTML = 'CLICK!';
-    document.getElementById('box2').style.backgroundColor = "orange";
-};
+document.getElementById('box1').addEventListener('click', changeColor1);
+document.getElementById('box2').addEventListener('click', changeColor2);
+document.getElementById('box3').addEventListener('click', changeColor3);
+
+
+
+function changeColor1(){
+    document.getElementById('box1').innerHTML = 'You clicked me!';
+    document.getElementById('box1').style.backgroundColor="orange";
+}
+
+function changeColor2(){
+    document.getElementById('box2').innerHTML='You clicked me!';
+    document.getElementById('box2').style.backgroundColor='purple'; 
+}
+
+function changeColor3(){
+    document.getElementById('box3').innerHTML='You clicked me!';
+    document.getElementById('box3').style.backgroundColor='green';
+}
+
+
+
+

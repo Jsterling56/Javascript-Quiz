7ae19a9c0751623d75e4b5ec590b2aa341d09dc1
unknown <jtsterlin@yahoo.com>
2023-05-08 22:31:58 -0400

    added JS+HTML framework, clarified CSS

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 document.getElementById('box1').addEventListener('click', changeColor1);
 document.getElementById('box2').addEventListener('click', changeColor2);
 document.getElementById('box3').addEventListener('click', changeColor3);
+document.getElementById('box4').addEventListener('click', changeColor4);
 
+function startGame(){
 
+}
+
+function question1(){
+
+}
+
+function question2(){
+
+}
+
+function question3(){
+
+}
+
+function question4(){
+
+}
+
+function question5(){
+
+}
 
 function changeColor1(){
     document.getElementById('box1').innerHTML = 'You clicked me!';
@@ -17,6 +40,11 @@ function changeColor2(){
 function changeColor3(){
     document.getElementById('box3').innerHTML='You clicked me!';
     document.getElementById('box3').style.backgroundColor='green';
+}
+
+function changeColor4(){
+    document.getElementById('box4').innerHTML='You clicked me!';
+    document.getElementById('box4').style.backgroundColor='red';
 }
 
 
